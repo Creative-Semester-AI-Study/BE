874e7cf8319b9f12202681209b6ae933b0dacefc
hagyeong kim <hagyeong032p@naver.com>
2024-10-02 00:25:32 +0900

    postman 확인 위해서 spring security부분 일단 주석 처리&user 부분 겹쳐서 일단 지움

--- src/main/java/com/sejong/aistudyassistant/user/UserController.java
+++
@@ -1,7 +0,0 @@
-package com.sejong.aistudyassistant.user;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class UserController {
-}
--- src/main/java/com/sejong/aistudyassistant/user/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.sejong.aistudyassistant.user;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class UserService {
-}

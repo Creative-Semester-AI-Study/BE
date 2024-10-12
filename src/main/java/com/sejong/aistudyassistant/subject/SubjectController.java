afff3d0027105208f8266aca28bb2d4ba92d1219
hagyeong kim <hagyeong032p@naver.com>
2024-10-12 15:55:42 +0900

    fix: SubjectController 경로 api명세서 내용대로 수정

--- src/main/java/com/sejong/aistudyassistant/subject/SubjectController.java
+++ src/main/java/com/sejong/aistudyassistant/subject/SubjectController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/subject")
+@RequestMapping("/study/myPage")
 public class SubjectController {
 
     @Autowired

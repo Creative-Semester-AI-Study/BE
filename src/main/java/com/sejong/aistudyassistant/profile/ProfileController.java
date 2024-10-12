943a92c4bca62812693ecb60cdd955496e128254
hagyeong kim <hagyeong032p@naver.com>
2024-10-12 15:52:14 +0900

    fix: ProfileController 경로 api명세서 내용대로 수정

--- src/main/java/com/sejong/aistudyassistant/profile/ProfileController.java
+++ src/main/java/com/sejong/aistudyassistant/profile/ProfileController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/profile")
+@RequestMapping("/study/profile")
 public class ProfileController {
 
     @Autowired

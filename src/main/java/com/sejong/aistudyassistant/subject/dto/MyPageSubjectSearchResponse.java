886db0d7516dd66a770e0f0cbf503d887b8a5001
hagyeong kim <hagyeong032p@naver.com>
2024-11-13 14:43:03 +0900

    fix: merge 완료

--- src/main/java/com/sejong/aistudyassistant/subject/dto/MyPageSubjectSearchResponse.java
+++ src/main/java/com/sejong/aistudyassistant/subject/dto/MyPageSubjectSearchResponse.java
@@ -1,23 +1,23 @@
 package com.sejong.aistudyassistant.subject.dto;
 
-import com.sejong.aistudyassistant.text.Text;
+//import com.sejong.aistudyassistant.text.Text;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 public class MyPageSubjectSearchResponse {
     private boolean status;
-    private List<Text> textResponses; // 텍스트 데이터를 담을 리스트 추가
+    //private List<Text> textResponses; // 텍스트 데이터를 담을 리스트 추가
 
     // 기본 생성자
     public MyPageSubjectSearchResponse() {
     }
 
     // 상태와 텍스트 리스트를 초기화하는 생성자
-    public MyPageSubjectSearchResponse(boolean status, List<Text> textResponses) {
-        this.status = status;
-        this.textResponses = textResponses;
-    }
+//    public MyPageSubjectSearchResponse(boolean status, List<Text> textResponses) {
+//        this.status = status;
+//        this.textResponses = textResponses;
+//    }
 
     public boolean isStatus() {
         return status;
@@ -27,13 +27,13 @@ public class MyPageSubjectSearchResponse {
         this.status = status;
     }
 
-    public List<Text> getTextResponses() {
-        return textResponses;
-    }
+//    public List<Text> getTextResponses() {
+//        return textResponses;
+//    }
 
-    public void setTextResponses(List<Text> textResponses) {
-        this.textResponses = textResponses;
-    }
+//    public void setTextResponses(List<Text> textResponses) {
+//        this.textResponses = textResponses;
+//    }
 
     public void setTextTransforms(List<Object> collect) {
     }
